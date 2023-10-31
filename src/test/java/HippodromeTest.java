0cfae78d7f9ce671745549359d7777e4f97a5967
nikita <nikitachertkov@gmail.com>
2023-10-31 16:39:39 +0500

    test

---
+++ src/test/java/HippodromeTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HippodromeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getHorses() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void getWinner() {
+    }
+}
