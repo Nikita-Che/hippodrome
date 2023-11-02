6bb5863b16be908ad6878e1532b9b628f9e3478a
nikita <nikitachertkov@gmail.com>
2023-11-02 11:46:44 +0500

    with Razbor Zadaniya corected2

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,5 +12,12 @@ class MainTest {
         assertTimeout(java.time.Duration.ofSeconds(20), () -> {
             Main.main(new String[]{});
         });
+    }
+
+    @Disabled
+    @Test
+    @Timeout(value = 20)
+    public void timeOut () throws Exception {
+        Main.main(null);
     }
 }
